05845f85dfbc17884f0447422145c2de477d7950
Garyuten <garyuten@gmail.com>
2023-12-18 16:46:26 +0900

    edit ROOT_BASE

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 
+// 環境変数からベースURLを取得
+const base = process.env.VITE_ROOT_BASE || '/';
+
+
 // https://astro.build/config
 export default defineConfig({
   publicDir: "./public",
   // outDir: "./dist",
-  // base: "/",
+  base: base,
 
   site: "https://Garyuten.github.io/", // for GitHub Pages
   outDir: "./docs", // for GitHub Pages
-  base: process.env.VITE_ROOT_BASE, 
+  base: process.env.VITE_ROOT_BASE,
 });
