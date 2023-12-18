88366b5a49ddb3ec022042fd40a82384e2679481
Garyuten <garyuten@gmail.com>
2023-12-18 17:19:38 +0900

    Astro でリンクや参照などを相対パスでビルドする

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import relativeLinks from "astro-relative-links";
 
 // 環境変数からベースURLを取得
-const base = process.env.VITE_ROOT_BASE || "/Astro-baserCMS5/";
+// const base = process.env.VITE_ROOT_BASE || "/Astro-baserCMS5/";
+const base = "/Astro-baserCMS5/";
 console.log("base: " + base);
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: "./public",
   // outDir: "./dist",
-  base: base,
+  base: "/Astro-baserCMS5/",
+  integrations: [relativeLinks()],
 
   site: "https://Garyuten.github.io/", // for GitHub Pages
   outDir: "./docs", // for GitHub Pages
